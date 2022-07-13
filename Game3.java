2d14e49b626b6b342527b394152257ae2ba080e6
Taiki <t9034869@icloud.com>
2022-07-13 22:48:59 +0900

    Victory message added

---
+++ Game3.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+public class Game3 {
+  public static void main(String[] args){
+    int heads = 0;
+    int tails = 0;
+
+    System.out.println("Who are you?");
+    System.out.print(">");
+    Scanner sc = new Scanner(System.in);
+    String name = sc.next();
+    System.out.println("Hello," + name + "!");
+    System.out.println("Tossing a coin...");
+    for(int i = 1 ; i <= 3 ; i++){
+      System.out.print("Round" + i +":");
+      String round = sc.next();
+      if("Heads".equals(round)) heads++;
+      else tails++;
+    }
+    System.out.println("Heads:" + heads + ",Tails:" + tails);
+    if(heads > tails) System.out.println("You won");
+    else if(heads < tails) System.out.println("You lost");
+    else System.out.println("Equal");
+  }
+}
